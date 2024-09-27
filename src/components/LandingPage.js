8b65319d6774840c0947c890e8309feb74eb65c6
gashchukk <145265923+gashchukk@users.noreply.github.com>
2024-09-27 21:06:17 +0300

    landing page part1

---
+++ src/components/LandingPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './LandingPage.css'
+import {dividerClasses} from "@mui/material";
+export default function LandingPage() {
+    return (
+        <div className="landing-page">
+            <div className="landing-page-part1">
+            <h1 className="heading">Ac cras eu, nisl dui pulvinar<br/> malesuada est eget quam vitae<br/> ciras fames fermentum</h1>
+            <div className="landing-page-content1">
+                <div className="small-text1">
+                    <p>Ac cras eu, nisl dui pulvinar<br/> malesuada est eget quam vitae<br/> ciras fames fermentum</p>
+                </div>
+                <div className="small-text2">
+                    <p>Ac cras eu, nisl dui pulvinar<br/> malesuada est eget quam vitae<br/> ciras fames fermentum</p>
+                </div>
+                <div className="button1">
+                    <button className="button1">Try it for free</button>
+                </div>
+            </div>
+            </div>
+            <div className="landing-page-part2">
+
+            </div>
+        </div>
+        )
+}
+
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Banner from '../components/Banner'
+import LandingPage from "../components/LandingPage";
 export default function HomePage() {
     return (
         <div className="container">
             <Banner />
+            <LandingPage />
         </div>
 )
 }
